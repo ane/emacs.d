170407c27b9d0c5362c06ae51ee67dce9a53b705
ane <ane@iki.fi>
2015-03-26 09:27:48 +0200

    Remove numbers

--- settings.el
+++ settings.el
@@ -84,5 +84,4 @@
 ;; company settings
 (setq company-idle-delay 0.1
       company-minimum-prefix-length 2
-      company-selection-wrap-around t
-      company-show-numbers t)
+      company-selection-wrap-around t)
