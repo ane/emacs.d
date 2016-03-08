e143a954dc5fbddd3aadd2a06be21b0e716c0ca9
Antoine Kalmbach <ane@iki.fi>
2016-03-08 19:06:19 +0200

    Update this

--- config/go.el
+++ config/go.el
@@ -8,11 +8,12 @@
                           (setq gofmt-command "goimports")
                           (setq gofmt-show-errors nil)
                           (add-hook 'before-save-hook 'gofmt-before-save)
-                          (setq-local
-                           company-go-show-annotation t
-                           intent-tabs-mode t)
+                          (setq-local company-go-show-annotation t)
+                          (setq-local intent-tabs-mode t)
                           (set (make-local-variable 'company-backends) '(company-go))
                           (company-mode)))
 
 (add-hook 'speedbar-load-hook (lambda ()
                                 (push ".go" speedbar-supported-extension-expressions)))
+
+(add-hook 'go-mode-hook #'squeak-mode)
