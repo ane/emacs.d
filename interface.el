11dfda278aad1a0f5029ce5492d22d8b2dd38b3d
Antoine Kalmbach <antoine.kalmbach@qvantel.com>
2017-01-04 17:08:30 +0200

    Puts some updates into this baby

--- interface.el
+++ interface.el
@@ -87,11 +87,11 @@
 (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)                              ; when a daemon, invoke theme startup 
 (setq spaceline-workspace-numbers-unicode t)
 (setq spaceline-window-numbers-unicode t)
-(load-theme 'sanityinc-tomorrow-night t)
+(load-theme 'monokai t)
 
 (defun setup-interface ()
   (interactive)
-  (set-default-font (font-spec :family "Ubuntu Mono" :size 14.0))
+  (set-default-font (font-spec :family "Source Code Pro" :size 15.0))
   (global-evil-leader-mode +1)
   (global-evil-surround-mode +1)
   (evil-exchange-install)
@@ -99,6 +99,7 @@
   (evil-mode +1)
   (evil-vimish-fold-mode 1)
   (global-hl-line-mode)
+  (setq browse-url-browser-function 'browse-url-default-browser)
   (projectile-global-mode +1))
 
 (add-hook 'after-make-frame-functions
